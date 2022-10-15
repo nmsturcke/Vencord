9dfdb2013af48b8872a8b9759134a52fe3dcdde0
Snek <107999380+sneksnake@users.noreply.github.com>
2022-10-15 11:21:00 +0200

    added "locked" icon & enabled

--- src/plugins/showHiddenChannels.ts
+++ src/plugins/showHiddenChannels.ts
@@ -2,12 +2,17 @@ import definePlugin from "../utils/types";
 import { Channel } from "discord-types/general";
 import { findByProps } from "../webpack";
 
-var can = (a, b) => true;
+var can = (a, b) => true; // defaults to true while findByProps isn't loaded
+
 const VIEW_CHANNEL = 1024n;
 
+const VOICE_CHANNEL = 2;
+
+// https://github.com/Vendicated/Vencord/pull/101#issuecomment-1279601609
+var enabled = false;
 
 export default definePlugin({
-    name: "Show Hidden Channels",
+    name: "ShowHiddenChannels",
     description: "Shows hidden channels",
     authors: [
         {
@@ -22,28 +27,50 @@ export default definePlugin({
             find: ".CannotShow",
             replacement: {
                 match: /renderLevel:(\w+)\.CannotShow/g,
-                replace: "renderLevel:$1.Show"
+                replace: "renderLevel:Vencord.Plugins.plugins[\"ShowHiddenChannels\"].isEnabled()?$1.Show:$1.CannotShow"
             }
         },
         {
             // Begining of the function that creates the elements.
             // Changing the name here has a smaller footerprint than changing the permissions and real name
-            find: "e.channel,o=e.name,u=e.muted,c=e.selected",
+            find: "l=e.hasActiveThreads",
+            replacement:
+            {
+                match: /hasActiveThreads,(.)=function\((\w+),(\w+),(\w+)\){switch\((\w+.type)\)/g,
+                replace: "hasActiveThreads,$1=function($2,$3,$4){switch(Vencord.Plugins.plugins[\"ShowHiddenChannels\"].getChannelTypeIcon($2))"
+            }
+
+        },
+        {
+            // This is where the logic that chooses the icon is
+            find: ".rulesChannelId))",
             replacement: {
-                match: /(\w+)\.channel,(\w+)=(\w+)\.name,(\w+)=(\w+)\.muted,(\w+)=(\w+)\.selected/g,
-                replace: "$1.channel,$2=Vencord.Plugins.plugins[\"Show Hidden Channels\"].getNameForChannel($3.channel),$4=$5.muted,$6=$7.selected"
+                match: /switch\((\w+)\.type\){case (\w+\.\w+\.GUILD_ANNOUNCEMENT)/g,
+                replace: "i=$1._isHiddenChannel?true:i;switch($1._isHiddenChannel?2:$1.type){case $2"
             }
         }
     ],
-    getNameForChannel: (e: Channel) => {
-        if (can(VIEW_CHANNEL, e)) {
-            return e.name;
-        } else {
-            // This is a bit hacky, but it works alright
-            return "[🔒] " + e.name;
+    getChannelTypeIcon: (channel: Channel) => {
+        if (!enabled) {
+            (channel as any)._isHiddenChannel = false;
+            return channel.type;
         }
+
+        const canView = can(VIEW_CHANNEL, channel);
+
+        // It's a little crude but it doesn't break anything
+        (channel as any)._isHiddenChannel = !canView;
+
+        return canView ? channel.type : VOICE_CHANNEL;
     },
     start() {
         ({ can } = findByProps("can", "initialize"));
+        enabled = true;
+    },
+    stop() {
+        enabled = false;
+    },
+    isEnabled() {
+        return enabled;
     }
 });
